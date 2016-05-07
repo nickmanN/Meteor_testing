82c154b9d2fc3b25624c8765fb8d7f83074ee2b5
nickmanN <r.shopyak@gmail.com>
2016-05-07 21:57:17 +0300

    First test

---
+++ tests/features/register-new-user.feature
@@ -0,0 +1,30 @@
+Feature: Register new User
+
+ As a visitor to the site,
+ so that I can pass registration on website,
+ And to become registered user.
+
+ Background:
+    Given I am on the site
+
+ @watch
+ Scenario: Visitor make registration
+    When I click on "sign up" button
+    And see "Almost there, just sign up"
+    Then I click on input field "Your username/email" 
+    And enter my username or email
+    Then I click on input field "Your password"
+    And enter password
+    Then I click on input field "Confirm your password"
+    And enter same password again
+    Then I click on "Sign Up" button to submit form
+    Then "Dasboard" and "Admin" and "Sign Out" buttons appear
+
+
+
+
+# /tests/features/register-new-user.feature
+
+
+
+# /tests/features/create-widget.feature
---
+++ tests/features/register-new-user.js
@@ -0,0 +1,59 @@
+module.exports = function () {  
+ 'use strict';
+ var name = 'Peter';
+ var pass = 1;
+
+	  this.Given(/^I am on the site$/, function () {
+	    browser.url('http://localhost:3000');
+	  });
+
+	  this.When(/^I click on "([^"]*)" button$/, function (arg1) {
+	    browser.click('//*[@id="react-root"]/div/header/nav/ul/li[3]/a');
+	  });
+
+	  this.When(/^see "([^"]*)"$/, function (arg1) {
+	  	var text = 'h2=Almost there, just sign up';
+	  	browser.waitForVisible(text);
+	    expect(browser.getText(text)).toEqual(arg1);
+	  });
+
+	  this.Then(/^I click on input field "([^"]*)"$/, function (arg1) {
+	    //browser.click('//*[@id="react-root"]/div/div[3]/div/div[1]/form/input[1]');
+		browser.click('input[placeholder="Your username/email"]');
+
+	  });
+
+	  this.Then(/^enter my username or email$/, function () {
+	    browser.setValue('input[placeholder="Your username/email"]', name);
+		//browser.timeouts('implicit', 5000);
+	  });
+
+	  this.Then(/^enter password$/, function () {
+	    browser.setValue('input[placeholder="Your password"]', pass);
+	  });
+
+	  this.Then(/^enter same password again$/, function () {
+	    browser.setValue('input[placeholder="Confirm your password"]', pass);
+	  });
+	 
+
+	   this.Then(/^I click on "([^"]*)" button to submit form$/, function (arg1) {
+	    browser.click('//*[@id="react-root"]/div/div[3]/div/div[1]/form/a');
+ 	  });
+
+
+	  this.Then(/^"([^"]*)" and "([^"]*)" and "([^"]*)" buttons appear$/, function (arg1, arg2, arg3) {
+	    var button1 = 'a*=Dashboard', button2 = 'a*=Admin', button3 = 'a*=Sign Out', button4 = '*=sometext';
+		//	could be error "Username already exists. [403]"
+		//	//*[@id="react-root"]/div/div[3]/div/div[1]/span/div
+		browser.waitForVisible(button3);
+
+	    expect(browser.isExisting(button1)).toEqual(true);
+	    expect(browser.isExisting(button2)).toEqual(true);
+	    expect(browser.isExisting(button3)).toEqual(true);
+	  });
+
+
+};
+
+// /tests/features/create-widget_steps.js
